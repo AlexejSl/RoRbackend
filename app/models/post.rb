55dd346194962e90766af9eee979926cc97a3d64
Alexej Slezák <alexej.slezak14@gmail.com>
2024-02-08 14:04:43 +0100

    bug fix of posts could be added without validation

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ApplicationRecord
   belongs_to :user, foreign_key: :username, primary_key: :username
+  validates :username, presence: true
+  validates :title, presence: true
+  validates :text, presence: true
   has_many :comments, dependent: :destroy
 end
